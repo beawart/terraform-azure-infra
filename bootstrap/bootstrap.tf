74dc69b2330abb0e70cc361337d5d9738bea8345
tahaB <itaha@live.com>
2025-08-14 23:34:55 +1000

    initial commit

---
+++ bootstrap/bootstrap.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "rg-terraform-state"
+  location = "Australia East"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                            = "tfstateaccount123" # must be globally unique
+  resource_group_name             = azurerm_resource_group.tfstate.name
+  location                        = azurerm_resource_group.tfstate.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+
+  min_tls_version = "TLS1_2"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 0.13"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstateaccount123"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
