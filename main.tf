24214b44883e16a80af5f75b0ed1c0bdb2635100
tahaB <itaha@live.com>
2025-09-16 00:57:20 +1000

    update

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "storage_acc_baseline" {
   source              = "git::https://github.com/beawart/azure-monitoring-kit-community.git//modules/baseline?ref=feature/tb/InitialUpdates"
   resource_group_name = "tfstate-rg"
   target_resource_id  = "/subscriptions/3ff211f1-4d5d-4ed8-b3fc-858378f20b7d/resourceGroups/tfstate-rg/providers/Microsoft.Storage/storageAccounts/tfstateaccountbeawart"
-  action_group_id     = "/subscriptions/3ff211f1-4d5d-4ed8-b3fc-858378f20b7d/resourceGroups/tfstate-rg/providers/microsoft.insights/actiongroups/ag-taha-email"
+  action_group_id     = ["/subscriptions/3ff211f1-4d5d-4ed8-b3fc-858378f20b7d/resourceGroups/tfstate-rg/providers/microsoft.insights/actiongroups/ag-taha-email"]
 }
