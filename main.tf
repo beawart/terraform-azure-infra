191da75077de160df643f1650229a46651357cc1
tahaB <itaha@live.com>
2025-09-25 06:40:12 +1000

    updates

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-module "baseline_health_alerts" {
+module "baseline_health_alerts-StorageAccount" {
   source              = "git::https://github.com/beawart/azure-monitoring-kit-community.git//modules/baseline-health"
   resource_group_name = "tfstate-rg"
   target_resource_ids = [
     "/subscriptions/3ff211f1-4d5d-4ed8-b3fc-858378f20b7d/resourceGroups/tfstate-rg/providers/Microsoft.Storage/storageAccounts/tfstateaccountbeawart"
   ]
-  # resource_type    = "storage_account"
+  resource_type    = "storage_account"
   action_group_ids = ["/subscriptions/3ff211f1-4d5d-4ed8-b3fc-858378f20b7d/resourceGroups/tfstate-rg/providers/microsoft.insights/actiongroups/ag-taha-email"]
   tags             = { environment = "prod" }
 
@@ -20,4 +20,16 @@ module "baseline_health_alerts" {
   }
 }
 
+# module "baseline_health_alerts-VirtualMachine" {
+#   source              = "git::https://github.com/beawart/azure-monitoring-kit-community.git//modules/baseline-health"
+#   resource_group_name = "tfstate-rg"
+#   target_resource_ids = [
+#     "/subscriptions/3ff211f1-4d5d-4ed8-b3fc-858378f20b7d/resourceGroups/tfstate-rg/providers/Microsoft.Storage/storageAccounts/tfstateaccountbeawart"
+#   ]
+#   resource_type    = "virtual_machine"
+#   action_group_ids = ["/subscriptions/3ff211f1-4d5d-4ed8-b3fc-858378f20b7d/resourceGroups/tfstate-rg/providers/microsoft.insights/actiongroups/ag-taha-email"]
+#   tags             = { environment = "prod" }
 
+#   alerts_overrides = {
+#   }
+# }
