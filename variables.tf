7f6465db34950fe4d4b6208a3e26b22858de709b
tahaB <itaha@live.com>
2025-09-30 23:31:21 +1000

    update

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "admin_password" {
-  description = "Admin password for the VM"
-  type        = string
-  sensitive   = true
-}
-
 variable "subscription_id" {
   description = "Subscription ID value"
   type        = string
