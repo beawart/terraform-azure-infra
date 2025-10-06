f07195e94402fd2029c955b2a79fa73878b123a5
tahaB <itaha@live.com>
2025-10-06 23:21:26 +1100

    update

--- cheap_vm.tf
+++ cheap_vm.tf
@@ -34,7 +34,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "cheap-linux-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "B1s" # Cheapest burstable size
+  size                = "Standard_B1ls" # Cheapest burstable size
   admin_username      = "azureuser"
 
   network_interface_ids = [
